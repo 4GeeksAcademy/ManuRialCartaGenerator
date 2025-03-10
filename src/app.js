e253cc53d4652c6fde7e91bc9cc6572223e7f8e8
Manu Rial Amado <30892359+rialinho@users.noreply.github.com>
2025-03-10 19:45:26 +0000

    Ejercicio básico resuelto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  generateCard();
 };
+function generateCard() {
+  const figuras = ["&spades;", "&hearts;", "&diams;", "&clubs;"];
+  const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  
+  let randomFigura = figuras[Math.floor(Math.random() * figuras.length)];
+  let randomValor = valores[Math.floor(Math.random() * valores.length)];
+  
+  // Asigna el contenido de la carta
+  document.querySelector(".figura").innerHTML = randomFigura;
+  document.querySelector(".valor").innerHTML = randomValor;
+  document.querySelector(".figuraInvertido").innerHTML = randomFigura;
+  
+  // Accede al DOM para cambiar el color correctamente
+  let cardElement = document.getElementById("card");
+  if (randomFigura === "&hearts;" || randomFigura === "&diams;") {
+      cardElement.style.color = "red";
+  } else {
+      cardElement.style.color = "black";
+  }
+}
+
